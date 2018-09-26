8dc49c421b4ab15b16a5fe120580958b9aeffd4e
Juho Vepsalainen <bebraw@gmail.com>
2018-09-26 14:48:39 +0200

    chore: Set up webpack

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require("path");
+
+module.exports = () => ({
+  stats: "minimal",
+  module: {
+    rules: [
+      {
+        test: /\.s?css$/,
+        use: ["style-loader", "css-loader", "sass-loader"]
+      },
+      {
+        test: /\.js$/,
+        use: "babel-loader",
+        include: [path.join(__dirname, "src")]
+      },
+      {
+        test: /\.woff(2)?|\.ttf$|\.eot$/,
+        use: [
+          {
+            loader: "url-loader",
+            options: {
+              limit: 5000
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(gif|jpg|png|svg)$/,
+        use: "file-loader"
+      },
+      {
+        test: /\.(md|txt)$/,
+        use: "raw-loader"
+      }
+    ]
+  }
+});
