542d5eb2753ddeaa9b45cde2a24d3cda5099ac22
Juho Vepsalainen <bebraw@gmail.com>
2018-10-07 11:14:24 +0200

    chore: Render initial schedule

--- src/SchedulePage.jsx
+++ src/SchedulePage.jsx
@@ -1,5 +1,37 @@
 import React from "react";
+import connect from "./connect";
+import Schedule from "./Schedule.jsx";
 
-const IndexPage = () => <div>schedule goes here</div>;
+const SchedulePage = ({ schedule }) => (
+  <Schedule intervals={schedule && schedule.intervals} />
+);
 
-export default IndexPage;
+export default connect(
+  `
+  query PageQuery($conferenceId: ID!, $day: String!) {
+    schedule(conferenceId: $conferenceId, day: $day) {
+      day
+      description
+      intervals {
+        begin
+        end
+        sessions {
+          type
+          interval {
+            begin
+            end
+          }
+          title
+
+          ... on Talk {
+            speakers {
+              name
+            }
+          }
+        }
+      }
+    }
+  }
+`,
+  () => ({ day: "2018-10-19" })
+)(SchedulePage);
