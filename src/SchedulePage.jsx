082288530f9b1bd3ad54b2e5d536f5807d4c18e2
Juho Vepsalainen <bebraw@gmail.com>
2018-10-07 11:35:40 +0200

    chore: Add initial version of schedule
    Still needs tuning and print CSS.

--- src/SchedulePage.jsx
+++ src/SchedulePage.jsx
@@ -1,10 +1,58 @@
 import React from "react";
 import connect from "./connect";
+import Contacts from "./Contacts.jsx";
+import Sponsor from "./Sponsor.jsx";
 import Schedule from "./Schedule.jsx";
+import Logo from "./assets/logo.svg";
+import styles from "./css/badges.scss";
 
-const SchedulePage = ({ schedule }) => (
-  <Schedule intervals={schedule && schedule.intervals} />
+const SchedulePage = ({
+  schedule,
+  conference: {
+    goldSponsors = [],
+    silverSponsors = [],
+    bronzeSponsors = []
+  } = {}
+}) => (
+  <div className={styles.scheduleContainer}>
+    <img
+      src={Logo}
+      alt="GraphQL Finland 2018"
+      className={styles.scheduleLogo}
+    />
+    <div className={styles.scheduleContent}>
+      <Schedule intervals={schedule && schedule.intervals} />
+    </div>
+    <div className={styles.goldSponsors}>
+      <section className={styles.sponsorsList}>
+        <Contacts items={goldSponsors} render={Sponsor} />
+      </section>
+    </div>
+    <div className={styles.silverSponsors}>
+      <section className={styles.sponsorsList}>
+        <Contacts items={silverSponsors} render={Sponsor} />
+      </section>
+    </div>
+    <div className={styles.bronzeSponsors}>
+      <section className={styles.sponsorsList}>
+        <Contacts items={bronzeSponsors} render={Sponsor} />
+      </section>
+    </div>
+  </div>
 );
+
+const sponsorFragment = `
+  fragment SponsorFragment on Contact {
+    name
+    social {
+      homepage
+    }
+    about
+    image {
+      url
+    }
+  }
+`;
 
 export default connect(
   `
@@ -31,7 +79,20 @@ export default connect(
         }
       }
     }
+    conference(id: $conferenceId) {
+      goldSponsors {
+        ...SponsorFragment
+      }
+      silverSponsors {
+        ...SponsorFragment
+      }
+      bronzeSponsors {
+        ...SponsorFragment
+      }
+    }
   }
+
+  ${sponsorFragment}
 `,
   () => ({ day: "2018-10-19" })
 )(SchedulePage);
