8db68c92df60a3db7e2754b847bcbbc9ce3d66f0
Juho Vepsalainen <bebraw@gmail.com>
2018-10-10 12:14:30 +0300

    chore: Set up a dummy server

---
+++ api.js
@@ -0,0 +1,17 @@
+/* eslint-disable no-console */
+const { GraphQLServer } = require("graphql-yoga");
+
+const typeDefs = `
+  type Query {
+    hello(name: String): String!
+  }
+`;
+
+const resolvers = {
+  Query: {
+    hello: (_, { name }) => `Hello ${name || "World"}`
+  }
+};
+
+const server = new GraphQLServer({ typeDefs, resolvers });
+server.start(() => console.log("Server is running on localhost:4000"));
