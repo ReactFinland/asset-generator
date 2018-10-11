1b01a8a23c2834bae1413b22bbd042fe4c4756d6
Juho Vepsalainen <bebraw@gmail.com>
2018-10-11 12:37:21 +0300

    feat: Add touch support to the presentation
    Much better now.
    
    Closes #11.

--- containers/PresentationContainer.jsx
+++ containers/PresentationContainer.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import SwipeableViews from "react-swipeable-views";
+import { bindKeyboard } from "react-swipeable-views-utils";
 import connect from "./connect";
 import SponsorsContainer from "./SponsorsContainer.jsx";
 import SessionTitle from "../components/SessionTitle.jsx";
@@ -7,78 +9,37 @@ import logo from "../assets/colored-logo.svg";
 import styles from "./presentation.scss";
 import root from "window-or-global";
 
-class PresentationContainer extends React.Component {
-  state = {
-    slide: getSlide()
-  };
-  componentDidMount() {
-    document.addEventListener("keydown", this.onKeydown, false);
-  }
-  componentWillUnmount() {
-    document.removeEventListener("keydown", this.onKeydown, false);
-  }
-  onKeydown = ({ key }) => {
-    if (key === "ArrowRight") {
-      const { schedule } = this.props;
-      const nextSlide = Math.min(
-        parseInt(root.location.hash.slice(1)) + 1,
-        schedule.intervals.length
-      );
+const KeyboardSwipeableViews = bindKeyboard(SwipeableViews);
 
-      root.location = `${location.origin}${location.pathname}#${nextSlide}`;
-      this.setState({ slide: nextSlide });
-    }
-    if (key === "ArrowLeft") {
-      const previousSlide = Math.max(
-        parseInt(root.location.hash.slice(1)) - 1,
-        0
-      );
-
-      root.location = `${location.origin}${location.pathname}#${previousSlide}`;
-      this.setState({ slide: previousSlide });
-    }
-  };
-
-  render() {
-    const { schedule } = this.props;
-    const { slide } = this.state;
-
-    if (slide === 0) {
-      return <TitlePage />;
-    }
-
-    return (
-      <div className={styles.presentationContainer}>
-        <img
-          src={logo}
-          alt="GraphQL Finland 2018"
-          className={styles.presentationLogo}
-        />
-        <div className={styles.presentationContent}>
-          <SessionTitle {...getPresentation(schedule, slide - 1)} />
+const PresentationContainer = ({ schedule }) => (
+  <KeyboardSwipeableViews
+    enableMouseEvents
+    onChangeIndex={index => {
+      root.location = `${location.origin}${location.pathname}#${index}`;
+    }}
+  >
+    <div className={styles.presentationContainer}>
+      <TitlePage />
+    </div>
+    {schedule ? (
+      schedule.intervals.map((interval, index) => (
+        <div className={styles.presentationContainer} key={index}>
+          <img
+            src={logo}
+            alt="GraphQL Finland 2018"
+            className={styles.presentationLogo}
+          />
+          <div className={styles.presentationContent}>
+            <SessionTitle {...interval.sessions[0]} />
+          </div>
+          <SponsorsContainer />
         </div>
-        <SponsorsContainer />
-      </div>
-    );
-  }
-}
-
-function getPresentation(schedule, index) {
-  if (!schedule) {
-    return {};
-  }
-
-  // XXX: Better with lodash get as otherwise this can crash with bad data
-  return schedule.intervals[index].sessions[0];
-}
-
-function getSlide() {
-  if (!root.location) {
-    return 0;
-  }
-
-  return root.location.hash ? parseInt(root.location.hash.slice(1)) : 0;
-}
+      ))
+    ) : (
+      <React.Fragment />
+    )}
+  </KeyboardSwipeableViews>
+);
 
 export default connect(
   `
